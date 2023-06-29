d8504367a0685a73aab32c1f35a4bdd68d053c6f
Balakrishnan1352001 <85740153+Balakrishnan1352001@users.noreply.github.com>
2023-06-29 20:28:20 +0530

    Component Files added

---
+++ Components/NewsItem.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const NewsItem =(props)=> {
+
+  
+    let { title, description, imageurl, newsurl, author, date, source } =props; // destructuring
+    return (
+      <div className='my-3'>
+        <div className="card" >
+          <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+            <span className=" badge rounded-pill bg-danger">{source}</span>
+
+          </div>
+          <img src={!imageurl ? "https://static.toiimg.com/thumb/msid-101214642,width-1070,height-580,imgsize-83722,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg" : imageurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-body-secondary">By {!author ? "unknown" : author} on {new Date(date).toGMTString()} </small></p>
+            <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  
+}
+
+export default NewsItem
